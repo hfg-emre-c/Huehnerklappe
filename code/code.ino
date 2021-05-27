563b113e53e1187ce4fbfd25d89d8048b1f54f61
hfg-leonard-t <leonard.thillmann@hfg.design>
2021-05-27 10:46:35 +0200

    temperatursensor

--- code/code.ino
+++ code/code.ino
@@ -1,8 +1,14 @@
+#include <dht.h>
+#include "dht.h"
+#define dht_apin A2
+
 #define enablePin 11
 #define in1Pin 10
 #define in2Pin 9
 #define schalterOben A4
 #define schalterUnten A3
+
+dht DHT;
 
 char stringComplete = "";
 String inputString = "1";
@@ -12,21 +18,27 @@ boolean directionM;
 void setup() {
 
   Serial.begin(9600);
+  delay(500);
   pinMode(in1Pin, OUTPUT);
   pinMode(in2Pin, OUTPUT);
   pinMode(enablePin, OUTPUT);
   pinMode(schalterOben, INPUT_PULLUP);
   pinMode(schalterUnten, INPUT_PULLUP);
+  delay(1000);
 
 }
 
 void loop() {
-  
- //Serial.println("{\"trigger\" : \"B\", \"value\": \"0\"}");
-  //Serial.print("/n");
-  delay(100);
-  //Serial.println("{ trigger: \"T\", status: \"test\"}");
-  Serial.print("vumsadsdasdasdsa");
+
+  //TEMPERATUR
+  DHT.read11(dht_apin);
+    //Serial.print("temperature = ");
+    //Serial.print(DHT.temperature); 
+    //Serial.println("C  ");
+    Serial.println(DHT.temperature);
+    delay(2000);
+
+  //HOCH UND RUNTER SCHALTER
   boolean buttonState;
   int speedM = 250;
   int obenState = digitalRead(schalterOben);
@@ -48,13 +60,13 @@ void loop() {
       inputString = "";
       stringComplete = "";
       break;
-      
+
   }
 
   if (obenState == 0 && buttonState == 0 && directionM == 1) {
     setMotor(0, directionM);
     buttonState = 1;
-    
+
   }
 
   if (untenState == 0 && buttonState == 0 && directionM == 0) {
